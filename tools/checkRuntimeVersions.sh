e17267ee33be327bf84745e31763db6ea6cf3353
Phillip Smith <phs@expensify.com>
2020-12-04 09:40:19 +1100

    add conditional for desired versions depending on ubuntu release

--- tools/checkRuntimeVersions.sh
+++ tools/checkRuntimeVersions.sh
@@ -2,9 +2,16 @@
 
 installedNodeVersion=$(node -v)
 installedNpmVersion=$(npm -v)
-
 desiredNodeVersion="v14.10.0"
 desiredNpmVersion="6.14.8"
+
+# While we're transitioning from Ubuntu 16.04 to 20.04, we need to conditionally override this variable
+# depending on which Ubuntu release we're running this script on.
+if [[ "$(uname)" == "Linux" ]] ; then
+    if [[ "$(lsb_release -rs)" == "20.04" ]] ; then
+       desiredNodeVersion="v14.15.0"
+    fi
+fi
 
 if [ "$installedNodeVersion" != "$desiredNodeVersion" ]
 then
