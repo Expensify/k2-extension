9539c8f43c26e66d7ed7fb6f186d98955c2d9bb2
Tim Golen <tgolen@expensify.com>
2022-01-14 11:52:29 -0700

    Cleanup webpack config a little to get the dev server working

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,17 @@ const path = require('path');
 
 const webpackConfig = {
   entry: {
-    content: path.resolve(__dirname, './lib/js/content.js'),
-    events: path.resolve(__dirname, './lib/js/events.js')
+    content: './lib/js/content.js',
+    events: './lib/js/events.js'
+  },
+  devServer: {
+    open: false
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, '../../dist'),
-    publicPath: '/',
-  },
+    path: path.resolve(__dirname, './dist'),
+    publicPath: './dist'
+  }
 };
 
 module.exports = webpackConfig;
