8533172c850f2fc3fac901d068409880af3f75e9
Tim Golen <tgolen@expensify.com>
2022-01-18 13:43:28 -0700

    Have files output correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const webpackConfig = {
+  context: path.resolve(__dirname, '.'),
   entry: {
     content: './lib/js/content.js',
     events: './lib/js/events.js'
   },
   mode: 'production',
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, './dist')
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
   },
 
   plugins: [
     new CleanWebpackPlugin(),
+
+    // Copies favicons into the dist/ folder to use for unread status
+    new CopyPlugin({
+      patterns: [
+        {from: './assets/'},
+      ],
+    }),
   ],
 
   module: {
@@ -27,16 +36,6 @@ const webpackConfig = {
       {
         test: /\.js$/i,
         use: 'babel-loader',
-
-        /**
-         * Exclude node_modules except two packages we need to convert for rendering HTML because they import
-         * "react-native" internally and use JSX which we need to convert to JS for the browser.
-         *
-         * You can remove something from this list if it doesn't use JSX/JS that needs to be transformed by babel.
-         */
-        include: [
-          path.resolve(__dirname, 'lib/js'),
-        ],
       }
     ]
   }
