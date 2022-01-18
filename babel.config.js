3f56dd7c565ecc39e8e02518ab4d12db176af8d9
Tim Golen <tgolen@expensify.com>
2022-01-18 13:17:49 -0700

    Got babel running

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+const defaultPresets = ['@babel/preset-react', '@babel/preset-env'];
+const defaultPlugins = [
+  '@babel/plugin-transform-runtime',
+  '@babel/plugin-proposal-class-properties',
+
+  // We use `transform-class-properties` for transforming ReactNative libraries and do not use it for our own
+  // source code transformation as we do not use class property assignment.
+  'transform-class-properties',
+];
+
+module.exports = {
+  env: {
+    production: {
+      presets: defaultPresets,
+      plugins: [...defaultPlugins, 'transform-remove-console'],
+    },
+    development: {
+      presets: defaultPresets,
+      plugins: defaultPlugins,
+    },
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,45 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const webpackConfig = {
   entry: {
     content: './lib/js/content.js',
     events: './lib/js/events.js'
   },
+  mode: 'development',
   devServer: {
     open: false
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, './dist'),
-    publicPath: './dist'
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist')
   },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+
   module: {
     rules: [
+      // Load .html files as strings, used for underscore templates
       {
         test: /\.html$/i,
-        loader: 'html-loader'
+        use: 'html-loader'
       },
       // Transpiles and lints all the JS
       {
-        test: /\.js$/,
-        loader: 'babel-loader'
+        test: /\.js$/i,
+        use: 'babel-loader',
+
+        /**
+         * Exclude node_modules except two packages we need to convert for rendering HTML because they import
+         * "react-native" internally and use JSX which we need to convert to JS for the browser.
+         *
+         * You can remove something from this list if it doesn't use JSX/JS that needs to be transformed by babel.
+         */
+        include: [
+          path.resolve(__dirname, 'lib/js'),
+        ],
       }
     ]
   }
