9ca03bc5b99a93493db133470de6ec298c30e7b6
Tim Golen <tgolen@expensify.com>
2022-02-02 11:39:58 -0700

    Remove gulp files

--- gulp/error.beep.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function(err) {
-  console.error(err.stack || (err.message + ' ' + err.fileName + ':' + err.lineNumber));
-  console.error('\u0007');
-  this.emit('end');
-};
