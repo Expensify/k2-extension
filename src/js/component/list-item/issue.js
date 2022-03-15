99b742302c49ea4dca36184119f56ce2ceec7ccb
Alex Beaman <alexbeaman@expensify.com>
2022-03-15 10:21:51 +0200

    Update logic for isContributorAssigned and check if issue external

--- src/js/component/list-item/issue.js
+++ src/js/component/list-item/issue.js
@@ -13,6 +13,7 @@ const _ = require('underscore');
 
 module.exports = React.createClass({
     parseIssue() {
+        this.isExternal = _.some(this.props.data.labels, {name: 'External'}) ? <sup>E</sup> : null;
         this.isImprovement = _.findWhere(this.props.data.labels, {name: 'Improvement'}) ? <sup>I</sup> : null;
         this.isTask = _.findWhere(this.props.data.labels, {name: 'Task'}) ? <sup>T</sup> : null;
         this.isFeature = _.findWhere(this.props.data.labels, {name: 'NewFeature'}) ? <sup>F</sup> : null;
@@ -28,7 +29,7 @@ module.exports = React.createClass({
         this.isWaitingOnCustomer = _.findWhere(this.props.data.labels, {name: 'Waiting for customer'}) ? ' waiting-for-customer' : '';
         this.isHeld = this.props.data.title.toLowerCase().indexOf('[hold') > -1 ? ' hold' : '';
         this.isChallengeSent = _.findWhere(this.props.data.labels, {name: 'Take Home Challenge Sent'}) ? ' challenge-sent' : '';
-        this.isHelpWanted = _.findWhere(this.props.data.labels, {name: 'Help Wanted'}) ? ' help-wanted' : '';
+        this.isContributorAssigned = _.some(this.props.data.labels, {name: 'Exported'}) && !_.some(this.props.data.labels, {name: 'Help Wanted'}) ? ' contributor-assigned' : '';
         this.isUnderReview = _.find(this.props.data.labels, label => label.name.toLowerCase() === 'reviewing');
     },
     getClassName() {
@@ -44,7 +45,7 @@ module.exports = React.createClass({
             className += ' overdue';
         }
 
-        return className + this.isPlanning + this.isWaitingOnCustomer + this.isHeld + this.isChallengeSent + this.isHelpWanted;
+        return className + this.isPlanning + this.isWaitingOnCustomer + this.isHeld + this.isChallengeSent + this.isContributorAssigned;
     },
     render() {
         this.parseIssue();
@@ -57,6 +58,7 @@ module.exports = React.createClass({
                 {this.isWhatsNext}
                 {this.isNewhire}
                 {this.isWaitingForCustomer}
+                {this.isExternal}
                 {this.isImprovement}
                 {this.isTask}
                 {this.isFeature}
