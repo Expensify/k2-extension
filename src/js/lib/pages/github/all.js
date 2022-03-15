d28370630d54230e043d1e9762045a3c089137cd
Tim Golen <tgolen@expensify.com>
2022-03-15 10:31:46 -0600

    Remove WhatsNext navigation button

--- src/js/lib/pages/github/all.js
+++ src/js/lib/pages/github/all.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 import Base from './_base';
 import k2Button from '../../../template/button.github.k2.html';
-import whatsnextButton from '../../../template/button.github.whatsnext.html';
 
 /**
  * This class manages the things that happen on *every* GitHub page. All it's doing is adding links to the
@@ -23,13 +22,6 @@ export default function () {
         // Hardcode because it doesn't change, and depending on GitHub markup means
         // it breaks every so often
         const currentUrl = '/Expensify/Expensify';
-
-        // Insert the what's next button right after the pull request button in the navigation
-        // navigation if it's there. Also make sure to not show it multiple times
-        if (!$('nav.js-repo-nav li.k2-whatsnext').length) {
-            $('nav.js-repo-nav *[data-selected-links*="repo_pulls"]')
-                .parent().after(whatsnextButton({url: currentUrl}));
-        }
 
         // Insert the kernel button right after the pull request button in the
         // navigation if it's there. Also make sure to not show it multiple times
