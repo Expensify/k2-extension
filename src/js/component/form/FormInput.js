224515a786ca22ba2c5f8cd62cac4742ab669d31
Tim Golen <tgolen@expensify.com>
2022-05-02 15:26:21 -0600

    Refactor FormInput to be an ES6 class

--- src/js/component/form/FormInput.js
+++ src/js/component/form/FormInput.js
@@ -1,22 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default React.createClass({
+const propTypes = {
+    /** Information about this specific form input */
+    data: PropTypes.shape({
+        /** Whether or not is should be automatically focused or not */
+        focus: PropTypes.bool,
+
+        /** the HTML ID of the input */
+        id: PropTypes.string,
+
+        /** Whether or not the input is required for the HTML form */
+        required: PropTypes.bool,
+
+        /** A class that can be added to the input */
+        className: PropTypes.string,
+    }).isRequired,
+
+    /** The type of form input this is */
+    type: PropTypes.string,
+};
+const defaultProps = {
+    type: 'text',
+};
+
+class FormInput extends React.Component {
     componentDidMount() {
         if (!this.props.data.focus) {
             return;
         }
         this.input.focus();
-    },
+    }
+
     render() {
         return (
             <input
                 ref={el => this.input = el}
-                type={this.props.type || 'text'}
+                type={this.props.type}
                 htmlId={this.props.data.id}
                 name={this.props.data.id}
                 required={this.props.data.required}
                 className={this.props.data.className}
             />
         );
-    },
-});
+    }
+}
+
+FormInput.propTypes = propTypes;
+FormInput.defaultProps = defaultProps;
+
+export default FormInput;
