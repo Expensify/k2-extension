707e9fa22912083473ab5e2edf1aeb75f66a1c77
Tim Golen <tgolen@expensify.com>
2022-05-05 11:13:37 -0600

    Rename file to match export

--- src/js/module/dashboard/ListIssues.js
+++ src/js/module/dashboard/ListIssues.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as prefs from '../../lib/prefs';
-import Filters from './filters';
+import Filters from './Filters';
 import Tabs from '../../component/tabs/tabs';
 
 import StoreIssueAssigned from '../../store/issue.all.assigned';
