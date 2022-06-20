c7d55daa04cb803b038455de891d10f4aa4eb0b7
Tim Golen <tgolen@expensify.com>
2022-06-20 09:51:01 -0600

    Add PR data to Onyx

--- src/js/lib/actions/PullRequests.js
+++ src/js/lib/actions/PullRequests.js
@@ -3,17 +3,32 @@ import * as API from '../api';
 import ONYXKEYS from '../../ONYXKEYS';
 
 function getAssigned() {
-    API.getPullsByType('assignee');
+    API.getPullsByType('assignee')
+        .then((prs) => {
+            // Always use set() here because there is no way to remove issues from Onyx
+            // that get closed and are no longer assigned
+            ReactNativeOnyx.set(ONYXKEYS.PRS.ASSIGNED, prs);
+        });
 }
 
 function getAuthored() {
-    API.getPullsByType('author');
+    API.getPullsByType('author')
+        .then((prs) => {
+            // Always use set() here because there is no way to remove issues from Onyx
+            // that get closed and are no longer assigned
+            ReactNativeOnyx.set(ONYXKEYS.PRS.AUTHORED, prs);
+        });
 }
 
 function getReviewing() {
     // @TODO also need to combine this with reviewed-by
     API.getPullsByType('review-requested');
-    API.getPullsByType('reviewed-by');
+    API.getPullsByType('reviewed-by')
+        .then((prs) => {
+            // Always use set() here because there is no way to remove issues from Onyx
+            // that get closed and are no longer assigned
+            ReactNativeOnyx.set(ONYXKEYS.PRS.REVIEWING, prs);
+        });
 }
 
 export {
