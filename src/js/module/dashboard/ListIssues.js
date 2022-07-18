2bbc6e9e5b558d9bcb588119022fb59b6f2f7019
Tim Golen <tgolen@expensify.com>
2022-07-18 14:16:54 -0600

    Add a component for the legend specifically

--- src/js/module/dashboard/ListIssues.js
+++ src/js/module/dashboard/ListIssues.js
@@ -9,6 +9,7 @@ import ListPRsAssigned from './ListPRsAssigned';
 import ListPRsReviewing from './ListPRsReviewing';
 import ListIssuesEngineering from './ListIssuesEngineering';
 import ListIssuesDailyImprovements from './ListIssuesDailyImprovements';
+import Legend from './Legend';
 
 const propTypes = {
     /** The number of seconds to refresh the list of issues */
@@ -28,90 +29,13 @@ class ListIssues extends React.Component {
     signOut() {
         prefs.clear('ghToken');
         Preferences.setGitHubToken('');
-        window.location.reload(true);
+        window.location.reload();
     }
 
     render() {
         return (
             <div className="issueList">
-
-                <div className="legend">
-                    <button
-                        type="button"
-                        onClick={this.signOut}
-                        className="btn tooltipped tooltipped-sw"
-                        aria-label="Sign Out"
-                    >
-                        Sign Out
-                    </button>
-                    <br />
-                    <br />
-                    <a
-                        className="btn btn-primary"
-                        aria-label="New Issue"
-                        href="https://github.com/Expensify/Expensify/issues/new/choose"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        New Issue
-                    </a>
-
-                    <br />
-                    <div className="issue reviewing">Under Review</div>
-                    <div className="issue overdue">Overdue</div>
-                    <div className="issue planning">Planning</div>
-                    <div className="issue contributor-assigned">Contributor Assigned</div>
-                    <div className="issue">
-                        <sup>E</sup>
-                        {' '}
-                        External
-                    </div>
-                    <div className="issue">
-                        <sup>I</sup>
-                        {' '}
-                        Improvement
-                    </div>
-                    <div className="issue">
-                        <sup>T</sup>
-                        {' '}
-                        Task
-                    </div>
-                    <div className="issue">
-                        <sup>F</sup>
-                        {' '}
-                        New Feature
-                    </div>
-                    <div>
-                        <span className="label hourly">H</span>
-                        {' '}
-                        Hourly
-                    </div>
-                    <div>
-                        <span className="label daily">D</span>
-                        {' '}
-                        Daily
-                    </div>
-                    <div>
-                        <span className="label weekly">W</span>
-                        {' '}
-                        Weekly
-                    </div>
-                    <div>
-                        <span className="label monthly">M</span>
-                        {' '}
-                        Monthly
-                    </div>
-                    <div>
-                        <span className="label newhire">FP</span>
-                        {' '}
-                        First Pick
-                    </div>
-                    <div>
-                        <span className="label whatsnext">WN</span>
-                        {' '}
-                        WhatsNext
-                    </div>
-                </div>
+                <Legend onSignOut={this.signOut} />
 
                 <ListIssuesAssigned pollInterval={this.props.pollInterval} />
 
@@ -123,7 +47,7 @@ class ListIssues extends React.Component {
 
                 <Filters />
 
-                <ListIssuesEngineering pollInterval={this.props.pollInterval} />
+                <ListIssuesEngineering pollInterval={this.props.pollInterval * 2.5} />
             </div>
         );
     }
