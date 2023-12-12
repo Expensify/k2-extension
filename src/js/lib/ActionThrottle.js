0f88c26ad9e25b55572fc684acd60dcb3c4be734
Tim Golen <tgolen@expensify.com>
2023-12-12 11:43:22 -0700

    Expand a commend about when to use throttling

--- src/js/lib/ActionThrottle.js
+++ src/js/lib/ActionThrottle.js
@@ -8,6 +8,9 @@ const lastFetchTimestamps = {};
  * It was necessary to add this because GH ends up doing a lot of page refreshing, which causes components to re-render, which causes fetch() to be called
  * more times than intended. This repeated calling of the API results in API thresholds being hit wwith GH.
  *
+ * This should primarily be used in any action file that calls the GitHub API in an ongoing setTimeout loop. It's better to do it in the action file than in api.js because
+ * other areas of the code might make one-time calls to the API methods where the throttling isn't necessary.
+ *
  * @param {String} commandName must be a unique name for each action method that fetches data from the GH API and stores the results in Onyx
  * @param {Function} methodThatReturnsAPromise
  */
