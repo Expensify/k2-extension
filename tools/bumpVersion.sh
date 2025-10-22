1c99233028a23953d3d4c9805a825719eb842a81
rory <rory@expensify.com>
2025-10-22 16:28:51 -0700

    Use npm version command to bump package.json version

--- tools/bumpVersion.sh
+++ tools/bumpVersion.sh
@@ -7,38 +7,19 @@ readonly TOP
 
 readonly PACKAGE_JSON="$TOP/package.json"
 
-# Read the version from package.json as the "current version"
-CURRENT_VERSION="$(grep -E '"version": "([0-9\.]+)"' "$PACKAGE_JSON" | awk -F\" '{print $4}')"
+# Save the current package.json version to a variable
+CURRENT_VERSION="$(jq -r '.version' < "$PACKAGE_JSON")"
 readonly CURRENT_VERSION
 
-# Parse the version into major/minor/patch
-readonly MAJOR="${CURRENT_VERSION%%.*}"
-readonly REMAINING="${CURRENT_VERSION#*.}"
-readonly MINOR="${REMAINING%%.*}"
-readonly PATCH="${CURRENT_VERSION##*.}"
+# Bump the version in package.json and package-lock.json
+npm --no-git-tag-version version patch
 
-# Increment the patch version
-NEW_MAJOR="$MAJOR"
-NEW_MINOR="$MINOR"
-NEW_PATCH="$((PATCH + 1))"
+# Save the new package.json version to a variable
+NEW_VERSION="$(jq -r '.version' < "$PACKAGE_JSON")"
+readonly NEW_VERSION
 
-# If patch is > 99, bump minor and reset patch to 0
-if [[ "$NEW_PATCH" -gt 99 ]] ; then
-    NEW_PATCH=0
-    NEW_MINOR="$((NEW_MINOR + 1))"
-fi
-
-# If minor is > 99, bump major and reset minor to 0
-if [[ "$NEW_MINOR" -gt 99 ]] ; then
-    NEW_MINOR=0
-    NEW_MAJOR="$((NEW_MAJOR + 1))"
-fi
-
-# Save the new version to a variable
-readonly NEW_VERSION="$NEW_MAJOR.$NEW_MINOR.$NEW_PATCH"
-
-# Bump the version in all the files
-for FILE in "$PACKAGE_JSON" "$TOP/package-lock.json" "$TOP/assets/manifest.json" "$TOP/assets/manifest-firefox.json" ; do
+# Bump the version in all the other manifest files
+for FILE in "$TOP/assets/manifest.json" "$TOP/assets/manifest-firefox.json" ; do
     # Replace only the first instance of $CURRENT_VERSION in case another package has the same version number
     sed -i -E '0,/"'"$CURRENT_VERSION"'"/s//"'"$NEW_VERSION"'"/' "$FILE"
 done
