abfc42d8b1377ae8050caa5928da52d311278747
rory <rory@expensify.com>
2025-10-22 16:56:50 -0700

    Fix version bumping overflow

--- tools/bumpVersion.sh
+++ tools/bumpVersion.sh
@@ -8,11 +8,30 @@ readonly TOP
 readonly PACKAGE_JSON="$TOP/package.json"
 
 # Save the current package.json version to a variable
-CURRENT_VERSION="$(jq -r '.version' < "$PACKAGE_JSON")"
-readonly CURRENT_VERSION
+ORIGINAL_VERSION="$(jq -r '.version' < "$PACKAGE_JSON")"
+readonly ORIGINAL_VERSION
 
 # Bump the version in package.json and package-lock.json
 npm --no-git-tag-version version patch
+CURRENT_VERSION="$(jq -r '.version' < "$PACKAGE_JSON")"
+
+# Auto-bump to next semver level if we reached 100
+# Extract version parts
+IFS='.' read -r _ MINOR PATCH <<< "$CURRENT_VERSION"
+
+# Check if patch is 100, if so bump minor
+if [[ "$PATCH" == "100" ]]; then
+    npm --no-git-tag-version version minor
+    CURRENT_VERSION="$(jq -r '.version' < "$PACKAGE_JSON")"
+    IFS='.' read -r _ MINOR PATCH <<< "$CURRENT_VERSION"
+fi
+
+# Check if minor is 100, if so bump major
+if [[ "$MINOR" == "100" ]]; then
+    npm --no-git-tag-version version major
+    CURRENT_VERSION="$(jq -r '.version' < "$PACKAGE_JSON")"
+fi
+
 
 # Save the new package.json version to a variable
 NEW_VERSION="$(jq -r '.version' < "$PACKAGE_JSON")"
@@ -21,7 +40,7 @@ readonly NEW_VERSION
 # Bump the version in all the other manifest files
 for FILE in "$TOP/assets/manifest.json" "$TOP/assets/manifest-firefox.json" ; do
     # Replace only the first instance of $CURRENT_VERSION in case another package has the same version number
-    sed -i -E '0,/"'"$CURRENT_VERSION"'"/s//"'"$NEW_VERSION"'"/' "$FILE"
+    sed -i -E '0,/"'"$ORIGINAL_VERSION"'"/s//"'"$NEW_VERSION"'"/' "$FILE"
 done
 
 # Print the new version if running interactively, or else print the variable for the GH action
